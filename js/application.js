4cf259d2cfd2e5010cfd8ffbbbfab985a3bee06f
mark <mhurwitzus@gmail.com>
2012-04-29 15:39:56 -0500

    added user creation code from tutorial

--- js/application.js
+++ js/application.js
@@ -3,5 +3,18 @@ $(document).ready(function(){
 	console.log('custom application.js loaded successfully!')
 
 
-	
+	  //Creating a user, but this time with a callback.
+  var user = new StackMob.User({ username: 'Bill Watterson', password: 'weirdosfromanotherplanet', profession: 'cartoonist'  });
+  user.create({
+    //After StackMob successfully saves "Bill Watterson", print out the result
+    success: function(model) {
+      //Print out "Bill Watterson: cartoonist"
+      console.debug(model.get('username') + ': ' + model.get('profession'));
+    },
+    error: function(model, response) {
+      console.debug("curses! we have failed, Hobbes!");
+    }
+  });
+
+  
 });
